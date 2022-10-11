3db05cdb43ea540cdbe50c54a9fd4495b8d9db4c
Harsh9724 <harsh972438@gmail.com>
2022-10-11 16:07:03 +0530

    list

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 
-function submit() {
-    var starter = document.querySelectorAll('input[name="starter[]"]:checked');
-    var main = document.querySelectorAll('input[name="main[]":checked');
-
-    document.getElementById('list1').innerHTML = "";
-    document.getElementById('list2').innerHTML = "";
-
-    for (var checkbox of starter) {
-        document.getElementById('list1').innerHTML += '<li>' + checkbox.value + '</li>';
-    }
-    for (var checkbox of main) {
-        document.getElementById('list2').innerHTML += '<li>' + checkbox.value + '</li>';
-    }
-}
-
-function Clean() {
-    document.getElementById('data').innerHTML = '';
-
-}
-
-
 const body = document.querySelector("body"),
     nav = document.querySelector("nav"),
     modeToggle = document.querySelector(".dark-light"),
@@ -78,3 +57,43 @@ for (i = 0; i < list.length; i++) {
     });
 }
 
+function sub() {
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    var starter = document.querySelectorAll('input[name="starter[]"]:checked');
+    var main = document.querySelectorAll('input[name="main[]"]:checked');
+
+    document.getElementById('list1').innerHTML = "";
+    document.getElementById('list2').innerHTML = "";
+
+    document.getElementById('data').style.visibility = "visible";
+
+    for (var checkbox of starter) {
+        document.getElementById('list1').innerHTML += '<li>' + checkbox.value + '</li>';
+    }
+    for (var checkbox of main) {
+        document.getElementById('list2').innerHTML += '<li>' + checkbox.value + '</li>';
+    }
+}
