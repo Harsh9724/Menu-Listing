bdee2c9f1f0a027c79830534f66ad6e7a2e07939
Harsh9724 <harsh972438@gmail.com>
2022-10-11 14:47:24 +0530

    not working

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 
 function submit() {
-
     var starter = document.querySelectorAll('input[name="starter[]"]:checked');
-    var dessert = document.querySelectorAll('input[name="dessert[]"]:checked');
-    var starter1 = document.querySelectorAll('input[name="starter1[]"]:checked');
+    var main = document.querySelectorAll('input[name="main[]":checked');
 
     document.getElementById('list1').innerHTML = "";
     document.getElementById('list2').innerHTML = "";
-    document.getElementById('list3').innerHTML = "";
-
-    document.getElementById('data').style.visibility = 'visible';
 
     for (var checkbox of starter) {
         document.getElementById('list1').innerHTML += '<li>' + checkbox.value + '</li>';
     }
-    for (var checkbox of starter1) {
-        document.getElementById('list3').innerHTML += '<li>' + checkbox.value + '</li>';
-    }
-    for (var checkbox of dessert) {
+    for (var checkbox of main) {
         document.getElementById('list2').innerHTML += '<li>' + checkbox.value + '</li>';
     }
 }
 
-function generatePDF() {
-    html2pdf().from(document.getElementById("data")).save();
-}
-
 function Clean() {
     document.getElementById('data').innerHTML = '';
-    document.getElementById('data').style.visibility = 'hidden';
 
 }
 
